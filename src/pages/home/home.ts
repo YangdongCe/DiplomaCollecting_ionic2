90daca81de19a88d7a0a65cb7d95c47848d2f103
lijinxuana <837127335@qq.com>
2017-05-06 18:10:58 +0800

    修改Home
    修改Home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { HomeservePage } from '../homeserve/homeserve';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+   items = [];
+   topic:string;
+   description:string;
+   content:string;
+   photo:URL;
 
   constructor(public navCtrl: NavController) {
 
-  }
-
+}
 }
