1d588ac0e09c9d53d6dfdb04bd587dfff8720a16
YangdongCe <yangdongce@imudges.com>
2017-05-24 00:19:29 +0800

    修改完新闻细节浏览页

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule, ErrorHandler } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule,DomSanitizer } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
@@ -50,6 +50,7 @@ import { NewsPage } from '../pages/newspage/newspage'
   imports: [
     BrowserModule,
     HttpModule,
+    
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/newspage/newspage.ts
+++ src/pages/newspage/newspage.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component,Injectable } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import { DomSanitizer } from '@angular/platform-browser'
 @Component({
    selector: 'page-newspage',
   templateUrl: 'newspage.html'
 })
 export class NewsPage {
 item: any;
-  constructor(public navCtrl: NavController, public navp: NavParams) {
+content: any;
+html;
+  constructor(public navCtrl: NavController, public navp: NavParams, public sanitizer: DomSanitizer ) {
     this.item=navp.get("item");
+    this.content = sanitizer.bypassSecurityTrustHtml(this.item.content);
+    this.html = this.assembleHTMLItem();
   }
+  assembleHTMLItem() {
+  this.content = this.sanitizer.bypassSecurityTrustHtml(this.item.content);
+  return this.content;
+}
 
 }
