982ce11c7f9f1fc436ad2aa8fb6fb09376ffa7c7
sangweijin <sangweijing@imudges.com>
2017-06-04 18:15:20 +0800

    修改home界面样式
    修改home界面样式

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -25,11 +25,7 @@ export class HomePage {
   }
   ngOnInit(){
     this.getPost();
-    this.newfist={
-      content:" <br>“一年之计在于春”，年头的时候大家都想为这一年做一些新的计划目标，例如考个资格证什么的！但是要注意哦！这几年来，有些资格证已经被国家取消了，所以当你想考某个资格证的时候，先看看这个资格证是否还考的必要！",
-      title:"2017年最新最全！这些职业资格证已经被国务院取消啦！不要再考了！",
-      source:"新闻网"
-    }
+    
   }
   getPost(){
     this.newlist.getPost().subscribe(reponse => {
